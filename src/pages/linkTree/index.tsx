28a3b00796e74b1a3d12bd3a1944f45e0707dd25
Bruno Leonardo Sancho Ventura <86660522+brunoventura123@users.noreply.github.com>
2022-05-14 15:46:45 -0300

    Link add

--- src/pages/linkTree/index.tsx
+++ src/pages/linkTree/index.tsx
@@ -1,5 +1,6 @@
 import * as C from './style'
 import She from '../../assets/images/she.png'
+import { Link } from 'react-router-dom'
 
 export const LinkTree = () => {
     return (
@@ -13,11 +14,11 @@ export const LinkTree = () => {
                 </div>
                 <div className="links">
                     <ul>
-                        <li><a href="/biotypeAnalysis">Análise de Estilo e Biótipo</a></li>
-                        <li><a href="/colorAnalysis">Análise de Coloração</a></li>
-                        <li><a href="/closetAnalysis">Análise de Guarda-roupa</a></li>
-                        <li><a href="/personalShopper">Personal Shopper</a></li>
-                        <li><a href="/lookMontages">Montagem de Looks</a></li>
+                        <li><Link to="/biotypeAnalysis">Análise de Estilo e Biótipo</Link></li>
+                        <li><Link to="/colorAnalysis">Análise de Coloração</Link></li>
+                        <li><Link to="/closetAnalysis">Análise de Guarda-roupa</Link></li>
+                        <li><Link to="/personalShopper">Personal Shopper</Link></li>
+                        <li><Link to="/lookMontages">Montagem de Looks</Link></li>
                     </ul>
                 </div>
             </div>
